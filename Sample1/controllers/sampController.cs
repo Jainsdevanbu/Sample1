645e8bf42748b2a175d18946ec75b07bad4e5ec2
Jains Devanbu <JA370294@wipro.com>
2018-07-04 19:46:10 +0530

    Importing Project

---
+++ Sample1/controllers/sampController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample1.controllers
+{
+ 
+    public class sampleController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/sample")]
+        public IActionResult Sample()
+        {
+            return Ok("Welcome to the sample page");
+           
+        }
+    }
+}
